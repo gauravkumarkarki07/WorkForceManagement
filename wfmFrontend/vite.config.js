d68f8cde258e4e938faa649a61ea31852116456a
gaurav <gauravkumarkarki07@gmail.com>
2024-06-05 21:31:51 +1000

    url changed proxy

--- wfmFrontend/vite.config.js
+++ wfmFrontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:'http://localhost:3000',
+        target:'https://workforcemanagement.onrender.com',
         secure:false
       }
     }
